e53a964b2d32769cd2c4d55f1a28e1ec99c0eeda
Joerg Hillebrand <hillebrand@jhillebrand.de>
2017-06-25 19:27:36 +0200

    Deduplication of TCM messages implemented

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
 #define VERSION_2               24
 #define VERSION                 "1.24.02"
-#define BUILD_DATE              "2017-06-18"
+#define BUILD_DATE              "2017-06-25"
 #define BUILD_NUMBER            "joehil build with Ventus mods"
 #define FW_NAME                 "a-culfw"
